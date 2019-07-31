07b2a858677488f542a9889d0973796bca53763c
Rina Razumov <rinash3@gmail.com>
2019-07-30 21:13:46 -0400

    created react app client

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes")
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -12,11 +17,16 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes)
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+//mongoose connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true
 });
 
 app.listen(PORT, () => {
